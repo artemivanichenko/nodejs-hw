fd09184ef7f030eb4c09218027079dfd9aa4de5b
Artem <119800207+artemivanichenko@users.noreply.github.com>
2026-01-13 15:12:02 +0100

    added avatar logic

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,11 @@ const userSchema = new Schema(
       trim: true,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        'https://png.pngtree.com/element_our/20200610/ourmid/pngtree-black-default-avatar-image_2237212.jpg',
+    },
   },
   {
     timestamps: true,
--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import noteRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
+import noteRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import multer from 'multer';
+
 // import { errors as celebrateErrorHandler } from 'celebrate';
 
 const app = express();
@@ -20,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/', noteRoutes);
 app.use('/auth', authRoutes);
-
+app.use('/user', userRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
